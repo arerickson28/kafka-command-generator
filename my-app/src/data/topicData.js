8d383ccd83d1a8077ad6dd3cfb5f88bd6456f235
arerickson28 <a.r.erickson.28@gmail.com>
2024-03-14 22:44:33 -0500

    adding dummy data

--- my-app/src/data/topicData.js
+++ my-app/src/data/topicData.js
@@ -4,10 +4,16 @@ const sslString = "ssl.location=<CHANGEME>"
 
 const topicData = [
     {
-        "name": "5Z",
+        "name": "1P",
         "producers": [
             {
                 "topicName": "my-awesome-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
+            },
+            {
+                "topicName": "my-luxurious-topic",
                 "localServer": "localhost:8080",
                 "stageServer": "theStageServer:9092",
                 "prodServer": "theProdServer:9093"
@@ -16,6 +22,12 @@ const topicData = [
         "consumers": [
             {
                 "topicName": "my-terrific-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
+            },
+            {
+                "topicName": "my-foolish-topic",
                 "localServer": "localhost:8080",
                 "stageServer": "theStageServer:9092",
                 "prodServer": "theProdServer:9093"
@@ -31,11 +43,54 @@ const topicData = [
                 "localServer": "localhost:8080",
                 "stageServer": "theStageServer:9092",
                 "prodServer": "theProdServer:9093"
+            },
+            {
+                "topicName": "my-hopless-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
             }
         ],
         "consumers": [
             {
                 "topicName": "my-fabulous-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
+            },
+            {
+                "topicName": "my-sultry-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
+            }
+        ]
+    },
+    {
+        "name": "5Z",
+        "producers": [
+            {
+                "topicName": "my-magnificent-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
+            },
+            {
+                "topicName": "my-curious-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
+            }
+        ],
+        "consumers": [
+            {
+                "topicName": "my-horrific-topic",
+                "localServer": "localhost:8080",
+                "stageServer": "theStageServer:9092",
+                "prodServer": "theProdServer:9093"
+            },
+            {
+                "topicName": "my-extravagant-topic",
                 "localServer": "localhost:8080",
                 "stageServer": "theStageServer:9092",
                 "prodServer": "theProdServer:9093"
