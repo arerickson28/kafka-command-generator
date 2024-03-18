a84821de822c96d5d0a18aea6527d61bb44e1883
RhyceErickson <Rhyce.Erickson@target.com>
2024-03-18 17:34:20 -0500

    move SSlLoc component to App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,24 @@ import SslLoc from './components/SslLoc';
 import styled from 'styled-components';
 import { topicData } from "./data/topicData"
 import store from "store"
+import {React, useState} from 'react';
 
+const TealBox = styled.div`
+    border: solid 4px rgb(0,128,128);
+    padding: 5px;
+    margin: 10px;
+    // display: flex;
+    // justify-content: center;
+`
 
+const PartsDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+
+const MyButton = styled.button`
+    margin: 15px;
+`
 
 function App() {
 
@@ -25,34 +41,109 @@ function App() {
   
   setLocalStorageIfNull(sslLoc)
 
+  const [isDisplayedState, setIsDisplayedState] = useState(false)
+  const [showUpdateBtnState, setShowUpdateBtnState] = useState(true)
+
+//    const [locInfoState, setLocInfoState] = useState({
+//         "ssl_certificate_location": "<changeMe>",
+//         "ssl_key_location": "<changeMe>",
+//         "ssl_ca_location": "<changeMe>"
+//     })
+
+  const [sslLocInfoState, setSslLocInfoState] = useState(store.get("sslLoc"))
+
+
+  function saveToLocalStorage() {
+      store.set("sslLoc", sslLocInfoState)
+  }
+
+
+  function toggleUpdateSSL() {
+      setIsDisplayedState(!isDisplayedState)
+      setShowUpdateBtnState(!showUpdateBtnState)
+  }
+
   return (
     <div className="App">
       <h1>Kafka Command Generator</h1>
       <hr></hr>
-      <SslLoc />
+      {/* <SslLoc /> */}
+      <div>
+        <TealBox>
+        <h3>Your SSL Location</h3>
+        <PartsDiv>
+        <h3>ssl.certificate.location = {sslLocInfoState.ssl_certificate_location}</h3>
+        <h3>ssl.key.location = {sslLocInfoState.ssl_key_location}</h3>
+        <h3>ssl.ca.location = {sslLocInfoState.ssl_ca_location}</h3>
+        </PartsDiv>
+        <hr></hr>
+        
+        {showUpdateBtnState ?  <MyButton onClick = {() => {toggleUpdateSSL()}}>Update</MyButton> : <></>}
+       
+        {
+                    isDisplayedState ?
+                    <>
+                    {/* <TealBox> */}
+                    <PartsDiv>
+                    {/* <div> */}
+                    <div>
+                    <label>ssl.certificate.location = </label>
+                    <input onInput={e => setSslLocInfoState({
+                        ...sslLocInfoState,
+                        "ssl_certificate_location": e.target.value
+
+                    })} type="text" />
+                    </div>
+
+                    <div>
+                    <label>ssl.key.location = </label>
+                    <input onInput={e => setSslLocInfoState({
+                        ...sslLocInfoState,
+                        "ssl_key_location": e.target.value
+
+                    })}type="text" />
+                    </div>
+                  
+                    <div>
+                    <label>ssl.ca.location = </label>
+                    <input onInput={e => setSslLocInfoState({
+                        ...sslLocInfoState,
+                        "ssl_ca_location": e.target.value
+
+                    })}type="text" />
+                    </div>
+                
+                    {/* </div> */}
+                   </PartsDiv>
+                   
+    
+                    <MyButton onClick = {() => {
+                        toggleUpdateSSL()
+                        saveToLocalStorage()
+                        } }>Save to local storage</MyButton>
+                    <br></br>
+                    <MyButton onClick = {() => {toggleUpdateSSL()} }>Cancel</MyButton>
+                    {/* </TealBox> */}
+                    </>
+                
+                    
+                    : <></> 
+                 
+                 }  
+        </TealBox>
+     
+        </div>
       {
         topicData.map((project) => {
           return (
             <>
             <hr></hr>
-              <Project projectData = {project} />
+              <Project projectData = {project} sslLocInfo = {sslLocInfoState}/>
               <hr></hr>
             </>
           )
         })
       }
-      {/* <hr></hr>
-      <Project projectName = "5Z" />
-      <hr></hr>
- 
-      <hr></hr>
-      <Project projectName = "8Q" />
-      <hr></hr>
-      <BodyDiv>
-      <Producers />
-      <Consumers />
-      </BodyDiv> */}
-  
     </div>
   );
 }
--- src/data/topicData.js
+++ src/data/topicData.js
@@ -1,6 +1,14 @@
 import { useBetween } from "use-between"
 
-const sslString = "ssl.location=<CHANGEME>"
+const sslString = "-X ssl.certificate.location=<changeMe> -X ssl.key.location=<changeMe> -X security.protocol=ssl -X ssl.ca.location=<changeMe>"
+
+const producerOrConsumer = "P/C"
+
+const broker = "kafka.app.dev.enterprise.com:9093"
+
+const topic = "my-awesome-topic"
+
+const baseString = `kcat -${producerOrConsumer} ${sslString} -b ${broker} -t ${topic}`
 
 const topicData = [
     {
@@ -9,28 +17,28 @@ const topicData = [
             {
                 "topicName": "my-awesome-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "awsomeStageServer:9092",
+                "prodServer": "awesomeProdServer:9093"
             },
             {
                 "topicName": "my-luxurious-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "luxuriousStageServer:9092",
+                "prodServer": "luxuriousProdServer:9093"
             }
         ],
         "consumers": [
             {
                 "topicName": "my-terrific-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "terrificStageServer:9092",
+                "prodServer": "terrificProdServer:9093"
             },
             {
                 "topicName": "my-foolish-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "foolishStageServer:9092",
+                "prodServer": "foolishProdServer:9093"
             }
         ]
     },
@@ -41,28 +49,28 @@ const topicData = [
             {
                 "topicName": "my-great-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "greatStageServer:9092",
+                "prodServer": "greatProdServer:9093"
             },
             {
-                "topicName": "my-hopless-topic",
+                "topicName": "my-hopeless-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "hopelessStageServer:9092",
+                "prodServer": "hopelessProdServer:9093"
             }
         ],
         "consumers": [
             {
                 "topicName": "my-fabulous-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "fabulousStageServer:9092",
+                "prodServer": "fabulousProdServer:9093"
             },
             {
                 "topicName": "my-sultry-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "sultryStageServer:9092",
+                "prodServer": "sultryProdServer:9093"
             }
         ]
     },
@@ -72,28 +80,28 @@ const topicData = [
             {
                 "topicName": "my-magnificent-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "magnificentStageServer:9092",
+                "prodServer": "magnificentProdServer:9093"
             },
             {
                 "topicName": "my-curious-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "curiousStageServer:9092",
+                "prodServer": "curiousProdServer:9093"
             }
         ],
         "consumers": [
             {
                 "topicName": "my-horrific-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "horrificStageServer:9092",
+                "prodServer": "horrificProdServer:9093"
             },
             {
                 "topicName": "my-extravagant-topic",
                 "localServer": "localhost:8080",
-                "stageServer": "theStageServer:9092",
-                "prodServer": "theProdServer:9093"
+                "stageServer": "extravagantStageServer:9092",
+                "prodServer": "extravagantProdServer:9093"
             }
         ]
     },
